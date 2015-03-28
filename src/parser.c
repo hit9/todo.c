16f5327dbfcff009f1ea2a6d227ecf726ec322c9
hit9 <hit9@icloud.com>
2015-03-28 23:45:42 +0800

    Update parser

--- src/parser.c
+++ src/parser.c
@@ -17,90 +17,76 @@
 #include "parser.h"
 
 static int
-is_space(uint8_t ch)
-{
-    return ch == ' ' || ch == '\t';
-}
-
-static uint8_t *
 skip_space(uint8_t *ch)
 {
-    while(is_space(*ch))
+    while (*ch == ' ' || *ch == '\t')
         ch++;
     return ch;
 }
 
-static unsigned int
-parse_line(todo_t *todo, uint8_t *start, size_t len, unsigned int lineno)
-{
-    if (lineno <= 0) {
-        // failed
-        return lineno;
-    }
-
-    int state = undo;
-
-    if (len == 0)
-        return 0;
-
-    uint8_t *ch = start;
-    uint8_t *data = NULL;
-    size_t size = 0;
-    task_t *task = NULL;
-
-    while (*(ch = skip_space(ch)) == '\n')
-        lineno++;
-
-    if (*ch == '-' && is_space(*++ch)) {
-        ch = skip_space(ch);
-
-        if (*ch++ = '[') {
-            ch = skip_space(ch);
-
-            if (*ch == 'x') {
-                state = done;
-                ch += 1;
-                ch = skip_space(ch);
-            }
-
-            if (*ch == ']') {
-                ch = skip_space(ch);
-
-                data = ch;
-
-                // one task, one line
-                while (*ch != '\n')
-                    ch++;
-
-                size = ch - data;
-                task = task_new(state, data, size);
-
-                if (task == NULL)
-                    return -1; // NOMEM
-
-                todo_push(todo, task);
-
-                ch += 1 && lineno += 1; // '\n'
-                return parse_line(todo, ch, len - (ch - start), lineno + 1);
-            }
-        }
-    }
-
-    return lineno;
-}
-
 todo_t *
-todo_parse(hbuf_t *buf) {
+todo_parse(hbuf_t *buf)
+{
     assert(buf != NULL);
 
     todo_t *todo = todo_new();
 
-    if (todo != NULL) {
-        int res = parse_line(todo, buf, 1);
+    if (todo == NULL)
+        return NULL;  // ENOMEM
 
-        if (res <= 0)
-            return NULL;
+    if (buf->size == 0)
         return todo;
+
+    task_t *task = NULL;
+    uint8_t *data = NULL;
+    size_t size = 0;
+    int state = undo;
+
+    uint8_t *ch = buf->data;
+    size_t lineno = 1;
+
+
+    while (ch < buf->data + buf->size) {
+        while (*(ch = skip_space(ch)) == '\n')
+            lineno++;
+
+        if (*ch != '[')
+            return NULL;  // ESNYTAXERR
+
+        ch = skip_space(ch);
+
+        switch (*ch) {
+            case 'x':
+                state = done;
+                ch += 1;
+                break;
+            case ']':
+                state = undo;
+                break;
+            default:
+                return NULL;  // EPARSEERR
+        }
+
+        ch = skip_space(ch);
+
+        if (*ch++ != ']')
+            return NULL;  // EPARSEERR
+
+        ch = skip_space(ch);
+
+        data = ch;
+
+        while (*ch++ != '\n');
+
+        size = ch - data;
+
+        if ((task = task_new(state, data, size)) == NULL)
+            return NULL;  // ENOMEM
+
+        todo_push(todo, task);
+
+        ch += 1;  // '\n'
+        lineno += 1;
     }
     return todo;
 }
