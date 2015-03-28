8f6b10e44027009dab99063d68ec1f80985e9f63
hit9 <hit9@icloud.com>
2015-03-29 05:02:46 +0800

    Add utils

--- src/utils.c
+++ src/utils.c
@@ -1,107 +1,71 @@
-/*
- * Copyright (c) 2013, hit9
+/**
+ * Copyright (c) 2015, Chao Wang (hit9 <hit9@icloud.com>)
  *
- * All rights reserved.
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright notice,
- *       this list of conditions and the following disclaimer in the documentation
- *       and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
- * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-
-
-/*
- * utils functions
- */
-
 
 #include "utils.h"
 
-#include <ctype.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/* wrap string color, remember to release the return string
- * after write it to stdout
+/**
+ * Test if file exists.
  */
-uint8_t *
-colored(uint8_t *str, size_t size, int color)
-{
-    /* white(37) as default */
-    if (color < 30 || color > 37)
-        color = 37;
-
-    const uint8_t *prefix = "\033[";  // 4 char
-    const uint8_t *suffix = "\033[0m";  // 6 char
-
-    /* the buffer's size */
-    size_t sz = (4 + 6 + 2 + 1) * sizeof(uint8_t) + size;
-
-    /* allocate enough buffer */
-    uint8_t *ret = (uint8_t *)malloc(sz * sizeof(uint8_t));
-
-    /* mix them in */
-    sprintf(ret, "%s%dm%.*s%s", prefix, color, (int)size, str, suffix);
-
-    return ret;
-}
-
-
-/* determine if a string is numberic */
-
 int
-is_int_like (const char * s)
+file_exists(const char *path)
 {
-    if (s == NULL || *s == '\0' || isspace(*s))
-      return 0;
-    char * p;
-    strtol (s, &p, 0);
-    return *p == '\0';
+    return access(path, F_OK) != -1;
 }
 
-/* join strings in array `arr` with ' ' */
-buf_t *
-space_join(uint8_t **arr, size_t len, buf_t *buf)
+/**
+ * Touch a file.
+ */
+int
+file_touch(const char *path)
 {
-    int i, str_len, sz;
-    uint8_t *str, *p;
+    FILE *fp = fopen(path, "w");
+    int res = (fp != NULL);
+    fclose(fp);
+    return res;
+}
 
-    for (i=0; i < len; i++) {
+/**
+ * Read file into buffer.
+ */
+int
+file_read(hbuf_t *buf, const char *path)
+{
+    assert(buf != NULL && path != NULL);
+    assert(buf->size <= buf->cap);
 
-        str = *(arr + i);
-        sz = str_len = strlen(str);
+    FILE *fp = fopen(path, "r");
 
-        /* if not the last array, append a empty space " " */
-        if (i != len-1) sz += 1;
+    if (fp == NULL)
+        return TD_EIO;
 
-        buf_grow(buf, buf->size + sz);
+    int bytes;
 
-        p = buf->data + buf->size;
+    while (1) {
+        if (hbuf_grow(buf, buf->size + FILE_READ_BUF_UNIT) != HBUF_OK)
+            return TD_ENOMEM;
 
-        strcpy(p, str);
+        bytes = fread(buf->data + buf->size, sizeof(uint8_t),
+                buf->cap - buf->size, fp);
 
-        /* if not the last array, append a empty space " " */
-        if (i != len-1) strcpy(p + str_len, " ");
+        if (bytes <= 0)
+            break;
 
-        buf->size += sz;
+        buf->size += bytes;
     }
 
-    return buf;
+    fclose(fp);
+    return TD_OK;
 }
--- src/utils.h
+++ src/utils.h
@@ -1,63 +1,45 @@
-/*
- * Copyright (c) 2013, hit9
+/**
+ * Copyright (c) 2015, Chao Wang (hit9 <hit9@icloud.com>)
  *
- * All rights reserved.
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- *     * Redistributions of source code must retain the above copyright notice,
- *       this list of conditions and the following disclaimer.
- *     * Redistributions in binary form must reproduce the above copyright notice,
- *       this list of conditions and the following disclaimer in the documentation
- *       and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER
- * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-
 
 #ifndef UTILS_H
 #define UTILS_H
 
+#include <assert.h>
 #include <stdint.h>
-#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-#include "buffer.h"
+#include "share.h"
+#include "hbuf.h"
 
-
-/* colors */
 typedef enum {
-    black = 30,
-    red = 31,
-    green = 32,
-    yellow = 33,
-    blue = 34,
+    black   = 30,
+    red     = 31,
+    green   = 32,
+    yellow  = 33,
+    blue    = 34,
     magenta = 35,
-    cyan = 36,
-    white = 37,
-} colors;
+    cyan    = 36,
+    white   = 37,
+} color_t;
 
-/*
- * all avaliable helpers
- */
-
-/* wrap string color */
-uint8_t *colored(uint8_t *str, size_t size, int color);
-
-/* test if a string is a valid integer format */
-int is_int_like(const char * s);
-
-/* join strings in array `arr` with ' ' */
-buf_t *space_join(uint8_t **arr, size_t len, buf_t *buf);
+int file_exists(const char *);
+int file_touch(const char *);
+int file_read(hbuf_t *, const char *);
 
 #endif
